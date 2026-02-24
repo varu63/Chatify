911a64572733a5681bd18a54e7a4ce638ceb0b36
Varun Tomar <varunrajput8077@gmail.com>
2026-02-24 19:21:10 +0530

    the full project are create ready for dep;oyment

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,16 +1,18 @@
-import express from "express"
+import express from "express";
+import { signup, login, logout, updateProfile } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { arcjetProtection } from "../middleware/arcjet.middleware.js";
 
 const router = express.Router();
 
-router.get("/signup" , (req , res)=>{
-    res.send("Signup endpoint")
-})
+router.use(arcjetProtection);
 
-router.get("/login" , (req , res)=>{
-    res.send("Login endpoint")
-})
-router.get("/logout" , (req , res)=>{
-    res.send("Logout endpoint")
-})
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/logout", logout);
 
-export default router
+router.put("/update-profile", protectRoute, updateProfile);
+
+router.get("/check", protectRoute, (req, res) => res.status(200).json(req.user));
+
+export default router;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,36 @@
 import express from "express";
-import dotenv from 'dotenv'
+import cookieParser from "cookie-parser";
+import path from "path";
+import cors from "cors";
 import authRoutes from "./routes/auth.routes.js";
-import messageRoutes from"./routes/message.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import { connectDB } from "./lib/db.js";
+import { ENV } from "./lib/env.js";
+import { app, server } from "./lib/socket.js";
+import dotenv from "dotenv";
 dotenv.config();
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+const __dirname = path.resolve();
 
-app.listen(3000 , ()=>console.log("Server running on port " + PORT));
+const PORT = ENV.PORT || 3000;
 
-app.use("/api/auth" , authRoutes);
-app.use("/api/message" , messageRoutes);
+app.use(express.json({ limit: "5mb" })); // req.body
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+// make ready for deployment
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+server.listen(PORT, () => {
+  console.log("Server running on port: " + PORT);
+  connectDB();
+});
